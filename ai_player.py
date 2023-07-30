0e505e01871830d0276936b8d69c78523ced39e6
jaerrib <jaerrib@tutanota.com>
2023-07-30 09:18:15 -0400

    Fix game sim bug; improve weight calculation
    - Simulation was changing players twice resulting in the same player making every move

--- ai_player.py
+++ ai_player.py
@@ -1,18 +1,12 @@
 from copy import deepcopy
-from game_logic import remaining_moves,\
-    assign_move,\
-    update_score,\
-    change_player,\
-    determine_winner,\
-    check_score
-
+from game_logic import remaining_moves,assign_move, determine_winner, check_score
 import secrets
 
 
 def assign_result_value(current_game):
     game_value = 0
-    score_p1 = check_score(current_game["board"], 1)
-    score_p2 = check_score(current_game["board"], 2)
+    score_p1 = check_score(current_game["board"], player=1)
+    score_p2 = check_score(current_game["board"], player=2)
     if current_game["result"] == "tie":
         game_value = 1
     elif current_game["result"] == "player 1":
@@ -20,7 +14,8 @@ def assign_result_value(current_game):
     elif current_game["result"] == "player 2":
         game_value = -2
     difference = abs(score_p1 - score_p2)
-    game_value = game_value * difference
+    adjustment = 48 - len(current_game["move_list"])
+    game_value = game_value * difference * adjustment
     return game_value
 
 
@@ -37,24 +32,18 @@ def sim_game_loop(data, players, depth):
         active_player = 1
     else:
         active_player = 2
-    first_row = 0
-    first_col = 0
     first_move = True
     temp_game["moves_left"] = remaining_moves(temp_game["board"])
     depth_counter = min(depth, len(temp_game["moves_left"]))
-
     while depth_counter >= 1:
         if players[active_player - 1] == "Computer":
-            temp_game["moves_left"] = remaining_moves(temp_game["board"])
             if len(temp_game["moves_left"]) >= 1:
                 ai_row, ai_col = computer_move(temp_game["moves_left"])
                 if first_move:
                     first_row = ai_row
                     first_col = ai_col
                     first_move = False
-                temp_game = assign_move(temp_game, ai_row, ai_col)
-                temp_game = update_score(temp_game)
-                temp_game = change_player(temp_game)
+        temp_game = assign_move(temp_game, ai_row, ai_col)
         depth_counter -= 1
 
     temp_game["result"] = determine_winner(temp_game["score_p1"], temp_game["score_p2"])
@@ -67,17 +56,18 @@ def sim_game_loop(data, players, depth):
 
 def get_best_move(data, sim_num, depth):
     temp_game = deepcopy(data)
-    best_score = 0
-    best_row = 0
-    best_col = 0
+    # Using -1000 simply ensures that losses and ties are scored
+    # better than the initial assignment
+    best_score = -1000
     players = ["Computer", "Computer"]
 
     for x in range(0, sim_num):
         returned_score, first_row, first_col = sim_game_loop(temp_game,
                                                              players,
                                                              depth)
-        if returned_score > best_score:
+        if returned_score >= best_score:
             best_score = returned_score
             best_row = first_row
             best_col = first_col
+    print(best_score)
     return best_row, best_col
