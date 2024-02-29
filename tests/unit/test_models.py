4ed444e9ae011f7a982a55e2e7f1fd2ab7e51371
jaerrib <jaerrib@tutanota.com>
2024-02-29 12:50:02 -0500

    Add game model creation test

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,5 +1,132 @@
+import unittest
+
 from app.game import Game
 
 
-def test_new_game():
-    game = Game()
+class TestGame(unittest.TestCase):
+
+    def test_new_game(self):
+        game = Game()
+        self.assertEqual(
+            game.board.data,
+            [
+                [
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (0, 0),
+                    (3, 3),
+                ],
+                [
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                    (3, 3),
+                ],
+            ],
+        )
+        self.assertEqual(game.move_list, [])
+        self.assertEqual(game.score_p1, 0)
+        self.assertEqual(game.score_p2, 0)
+        self.assertEqual(game.result, "")
+        self.assertEqual(game.active_player, 1)
+        self.assertEqual(game.active_stone, 1)
+        self.assertEqual(game.scoring_type, 1)
+        self.assertEqual(game.ruleset, "0.4")
+        self.assertEqual(
+            game.special_stones,
+            {
+                "player1": [1, 2, 3],
+                "player2": [1, 2, 3],
+            },
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
