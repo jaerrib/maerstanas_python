5a759d8387709b17e50bacce6579b3f3c9640777
jaerrib <jaerrib@tutanota.com>
2024-03-02 12:10:05 -0500

    Add more tests

--- tests/functional/test_game_logic.py
+++ tests/functional/test_game_logic.py
@@ -139,3 +139,34 @@ class GameLogicTest(unittest.TestCase):
         self.assertEqual(hinges, 2)
         hinges = hinge_check(self.game.board.data, row_number=4, col_number=5)
         self.assertEqual(hinges, 1)
+
+    def test_check_adjacent_stones(self):
+        # Check a known position that would cause a stone to have 4 hinges
+        creates_4_hinges = check_adjacent_stones(
+            self.game.board.data, row_number=6, col_number=3
+        )
+        self.assertEqual(creates_4_hinges, True)
+        # Check a known position that would cause a stone to have 4 hinges
+        creates_4_hinges = check_adjacent_stones(
+            self.game.board.data, row_number=2, col_number=1
+        )
+        self.assertEqual(creates_4_hinges, True)
+        # Check a known position that should not cause a stone to have 4 hinges
+        creates_4_hinges = check_adjacent_stones(
+            self.game.board.data, row_number=5, col_number=5
+        )
+        self.assertEqual(creates_4_hinges, False)
+
+    def test_check_score(self):
+        self.assertEqual(
+            check_score(board=self.game.board.data, score_type=0, player=1), 11
+        )
+        self.assertEqual(
+            check_score(board=self.game.board.data, score_type=0, player=2), 8
+        )
+        self.assertEqual(
+            check_score(board=self.game.board.data, score_type=1, player=1), 19
+        )
+        self.assertEqual(
+            check_score(board=self.game.board.data, score_type=1, player=2), 16
+        )
